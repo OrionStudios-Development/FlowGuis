e5553cb17fcf378b1b0d592e35534a3e32326d80
OrionStudios Development <mattia.carvelli72@gmail.com>
2026-01-18 19:46:51 +0100

    Delete src/main/java/it/orionstudios/flowgui directory

--- src/main/java/it/orionstudios/flowgui/context/GuiContext.java
+++
@@ -1,6 +0,0 @@
-package it.orionstudios.flowgui.context;
-
-import it.orionstudios.flowgui.FlowGui;
-import org.bukkit.entity.Player;
-
-public record GuiContext(Player player, int slot, FlowGui gui) {}
